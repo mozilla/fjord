bc6ec20692fcfa99e40a9f9300e8cd1ee3d2e693
Will Kahn-Greene <willg@bluesock.org>
2012-08-06 11:48:42 -0400

    Tweak setup.py info
    Adds classifiers, tweaks formatting, etc

--- setup.py
+++ setup.py
@@ -3,15 +3,23 @@ import os
 from setuptools import setup, find_packages
 
 
-setup(name='fjord',
-      version='1.0',
-      description='Django application.',
-      long_description='',
-      author='',
-      author_email='',
-      license='',
-      url='',
-      include_package_data=True,
-      classifiers = [],
-      packages=find_packages(exclude=['tests']),
-      install_requires=[])
+setup(
+    name='fjord',
+    version='1.0',
+    description='Django application.',
+    long_description='',
+    author='Mozilla Foundation',
+    author_email='',
+    license='BSD',
+    url='https://github.com/mozilla/fjord',
+    include_package_data=True,
+    classifiers=[
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        ],
+    packages=find_packages(exclude=['tests']),
+    install_requires=[],
+    )
