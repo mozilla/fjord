80e9cad2d5db807500bdaef844ebbf98e19be505
Will Kahn-Greene <willg@bluesock.org>
2012-09-06 14:22:09 -0400

    [bug 788527] Implement search index admin
    This version of the search index admin doesn't do much other than tell
    you what's going on index/mapping-type wise in your system.

---
+++ fjord/search/admin.py
@@ -0,0 +1,54 @@
+import logging
+from datetime import datetime
+
+from django.contrib import admin
+from django.shortcuts import render
+from django.template import RequestContext
+
+import pyes
+
+from fjord.search import utils
+from fjord.search.models import get_index
+
+
+log = logging.getLogger('i.search')
+
+
+def search_admin_view(request):
+    """Render the admin view containing search tools"""
+    error_messages = []
+    stats = None
+    indexes = []
+
+    try:
+        # This gets index stats, but also tells us whether ES is in
+        # a bad state.
+        try:
+            stats = utils.get_index_stats()
+        except pyes.exceptions.IndexMissingException:
+            stats = None
+        indexes = utils.get_indexes()
+        indexes.sort(key=lambda m: m[0])
+
+    except pyes.urllib3.MaxRetryError:
+        error_messages.append('Error: Elastic Search is not set up on this '
+                              'machine or is not responding. (MaxRetryError)')
+    except pyes.exceptions.IndexMissingException:
+        error_messages.append('Error: Index is missing. Press the reindex '
+                              'button below. (IndexMissingException)')
+    except pyes.urllib3.TimeoutError:
+        error_messages.append('Error: Connection to Elastic Search timed out. '
+                              '(TimeoutError)')
+
+    return render(request, 'admin/search_admin_view.html', {
+            'title': 'Search',
+            'mapping_type_stats': stats,
+            'indexes': indexes,
+            'index': get_index(),
+            'error_messages': error_messages,
+            'now': datetime.now(),
+            })
+
+
+admin.site.register_view('search-admin-view', search_admin_view,
+                         'Search - Index Maintenance')
