8ba726599ca642f06419fc200cbcd0eb37dfc68c
Will Kahn-Greene <willkg@mozilla.com>
2013-01-17 17:37:10 -0500

    PEP-8 pass.

--- fjord/analytics/views.py
+++ fjord/analytics/views.py
@@ -17,27 +17,30 @@ def counts_to_options(counts, name, display=None, display_map=None,
                       value_map=None, checked=None):
     """Generates a set of option blocks from a set of facet counts.
 
-    One options block represents a set of options to search for, as well as
-    the query parameter that can be used to search for that opinion, and the
-    friendly name to show the opinion block as.
+    One options block represents a set of options to search for, as
+    well as the query parameter that can be used to search for that
+    opinion, and the friendly name to show the opinion block as.
 
     For each option the keys mean:
     - `name`: Used to name in the DOM.
     - `display`: Shown to the user.
-    - `value`: The value to set the query parameter to in order to search for
-      this option.
+    - `value`: The value to set the query parameter to in order to
+      search for this option.
     - `count`: The facet count of this option.
     - `checked`: Whether the checkbox should start checked.
 
-    :arg counts: A list of tuples of the form (count, item), like from ES.
-    :arg name: The name of the search string that corresponds to this block.
-        Like "locale" or "platform".
-    :arg display: The human friendly title to represent this set of options.
-    :arg display_map: Either a dictionary or a function to map items to their
-        display names. For a dictionary, the form is {item: display}. For a
-        function, the form is lambda item: display_name.
-    :arg value_map: Like `display_map`, but for mapping the values that get put
-        into the query string for searching.
+    :arg counts: A list of tuples of the form (count, item), like from
+        ES.
+    :arg name: The name of the search string that corresponds to this
+        block.  Like "locale" or "platform".
+    :arg display: The human friendly title to represent this set of
+        options.
+    :arg display_map: Either a dictionary or a function to map items
+        to their display names. For a dictionary, the form is {item:
+        display}. For a function, the form is lambda item:
+        display_name.
+    :arg value_map: Like `display_map`, but for mapping the values
+        that get put into the query string for searching.
     :arg checked: Which item should be marked as checked.
     """
     if display is None:
@@ -49,13 +52,13 @@ def counts_to_options(counts, name, display=None, display_map=None,
         'options': [],
     }
 
-    # This is used in the loop below, to be a bit neater and so we can do it
-    # for both value and display generically.
+    # This is used in the loop below, to be a bit neater and so we can
+    # do it for both value and display generically.
     def from_map(source, item):
         """Look up an item from a source.
 
-        The source may be a dictionary, a function, or None, in which case the
-        item is returned unmodified.
+        The source may be a dictionary, a function, or None, in which
+        case the item is returned unmodified.
 
         """
         if source is None:
@@ -84,9 +87,9 @@ day_in_millis = 24 * 60 * 60 * 1000.0
 def _zero_fill(start, end, data_sets, spacing=day_in_millis):
     """Given one or more histogram dicts, zero fill them in a range.
 
-    The format of the dictionaries should be {milliseconds: numeric value}. It is
-    important that the time points in the dictionary are equally spaced. If they
-    are not, extra points will be added.
+    The format of the dictionaries should be {milliseconds: numeric
+    value}. It is important that the time points in the dictionary are
+    equally spaced. If they are not, extra points will be added.
 
     This method works with milliseconds because that is the format
     elasticsearch and Javascript use.
@@ -100,15 +103,15 @@ def _zero_fill(start, end, data_sets, spacing=day_in_millis):
     # Date ranges are inclusive on both ends.
     end_millis = epoch_milliseconds(end) + spacing
 
-    # `timestamp` is a loop counter that iterates over the timestamps from start
-    # to end. It can't just be `timestamp = start`, because then the zeros being
-    # adding to the data might not be aligned with the data already in the
-    # graph, since we aren't counting by 24 hours, and the data could have a
-    # timezone offset.
+    # `timestamp` is a loop counter that iterates over the timestamps
+    # from start to end. It can't just be `timestamp = start`, because
+    # then the zeros being adding to the data might not be aligned
+    # with the data already in the graph, since we aren't counting by
+    # 24 hours, and the data could have a timezone offset.
     #
-    # This block picks a time up to `spacing` time after `start` so that it
-    # lines up with the data. If there is no data, then we use `stamp =
-    # start`, because there is nothing to align with.
+    # This block picks a time up to `spacing` time after `start` so
+    # that it lines up with the data. If there is no data, then we use
+    # `stamp = start`, because there is nothing to align with.
 
     # start <= timestamp < start + spacing
     days = [d for d in data_sets if d.keys()]
@@ -121,8 +124,9 @@ def _zero_fill(start, end, data_sets, spacing=day_in_millis):
         # If there no data, it doesn't matter how it aligns.
         timestamp = start_millis
 
-    # Iterate in the range `start` to `end`, starting from `timestamp`, increasing
-    # by `spacing` each time. This ensures there is a data point for each day.
+    # Iterate in the range `start` to `end`, starting from
+    # `timestamp`, increasing by `spacing` each time. This ensures
+    # there is a data point for each day.
     while timestamp < end_millis:
         for d in data_sets:
             if timestamp not in d:
@@ -138,8 +142,10 @@ def dashboard(request, template):
     search_platform = request.GET.get('platform', None)
     search_locale = request.GET.get('locale', None)
     search_query = request.GET.get('q', None)
-    search_date_start = smart_datetime(request.GET.get('date_start', None), fallback=None)
-    search_date_end = smart_datetime(request.GET.get('date_end', None), fallback=None)
+    search_date_start = smart_datetime(request.GET.get('date_start', None),
+                                       fallback=None)
+    search_date_end = smart_datetime(request.GET.get('date_end', None),
+                                     fallback=None)
     selected = request.GET.get('selected', None)
 
     current_search = {'page': page}
@@ -169,7 +175,8 @@ def dashboard(request, template):
     current_search['date_end'] = search_date_end.strftime('%Y-%m-%d')
     # Add one day, so that the search range includes the entire day.
     end = search_date_end + timedelta(days=1)
-    # Note 'less than', not 'less than or equal', because of the added day above.
+    # Note 'less than', not 'less than or equal', because of the added
+    # day above.
     f &= F(created__lt=end)
 
     current_search['date_start'] = search_date_start.strftime('%Y-%m-%d')
@@ -186,9 +193,10 @@ def dashboard(request, template):
     facets = search.facet('happy', 'platform', 'locale',
         filtered=bool(f.filters))
 
-    # This loop does two things. First it maps 'T' -> True and 'F' -> False.
-    # This is probably something EU should be doing for us. Second, it
-    # restructures the data into a more convenient form.
+    # This loop does two things. First it maps 'T' -> True and 'F' ->
+    # False.  This is probably something EU should be doing for
+    # us. Second, it restructures the data into a more convenient
+    # form.
     counts = {'happy': {}, 'platform': {}, 'locale': {}}
     for param, terms in facets.facet_counts().items():
         for term in terms:
@@ -234,8 +242,10 @@ def dashboard(request, template):
 
     _zero_fill(search_date_start, search_date_end, [happy_data, sad_data])
     histogram = [
-        {'label': _('Happy'), 'name': 'happy', 'data': sorted(happy_data.items())},
-        {'label': _('Sad'), 'name': 'sad', 'data': sorted(sad_data.items())},
+        {'label': _('Happy'), 'name': 'happy',
+         'data': sorted(happy_data.items())},
+        {'label': _('Sad'), 'name': 'sad',
+         'data': sorted(sad_data.items())},
     ]
 
     # Pagination
