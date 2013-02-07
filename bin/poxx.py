6829e1b8707ecccd74bd52940453bd857a147997
Will Kahn-Greene <willkg@mozilla.com>
2013-02-07 14:59:00 -0500

    Switch poxx.py from "swedish chef" to pirate

--- bin/poxx.py
+++ bin/poxx.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Munge a .po file so we English-bound can see what strings aren't marked
-for translation yet.
+"""Munge a .po file so we English-bound can see what strings aren't
+marked for translation yet.
 
-Run this with a .po file as an argument.  It will set the translated strings
-to be the same as the English, but with vowels in the wrong case:
+Run this with a .po file as an argument.  It will set the translated
+strings to be the same as the English, but with vowels in the wrong
+case:
 
     ./poxx.py locale/xx/LC_MESSAGES/messages.po
 
@@ -15,98 +16,187 @@ Contributors:
 
 * Ned Batchelder
 * Will Kahn-Greene
+
 """
 
+import HTMLParser
+import optparse
 import re
 import string
 import sys
+
 try:
-    import polib    # from http://bitbucket.org/izi/polib
+    import polib  # from http://bitbucket.org/izi/polib
 except ImportError:
-    print "You need to install polib.  Do:"
-    print ""
-    print "   pip install polib"
+    print 'You need to install polib.  Do:'
+    print ''
+    print '   pip install polib'
     sys.exit()
-import HTMLParser
+
+
+USAGE = 'usage: %prog --test OR %prog -s STRING1 [...] OR %prog FILENAME1 [...]'
+
+
+DEBUG = False
 
 
 INTERP_RE = re.compile(
-    r'(%(?:[(].+?[)])?[#0 +-]?[.\d*]*[hlL]?[diouxXeEfFgGcrs%])')
+    r'((?:%(?:[(].+?[)])?[#0 +-]?[.\d*]*[hlL]?[diouxXeEfFgGcrs%])|(?:\{.+?\}))')
+
+
+COLOR = [
+    u'arr!',
+    u'arrRRr!',
+    u'arrRRRrrr!',
+    u'matey!',
+    u'ahoy!',
+    u'aye!',
+    u'ye scalleywag!',
+    u'cap\'n!',
+    u'yo-ho-ho!',
+    u'shiver me timbers!',
+    ]
+
+
+def debug(*args):
+    if DEBUG:
+        print ' '.join([str(arg) for arg in args])
 
 
 def wc(c):
-    return c == "'" or c in string.letters
+    return c == '\'' or c in string.letters
 
 
 def nwc(c):
     return not wc(c)
 
 
+def is_whitespace(s):
+    return re.match('^\\s*$', s) != None
+
+
+# List of transform rules. The tuples have:
+#
+# * in word
+# * not in word
+# * match string
+# * word character
+# * not word character
+# * replacement string
+#
+# See the transform code for details.
 TRANSFORM = (
     # INW?, NIW?, match, WC?, NW?, replacement
-    (False, False, "an", False, False, "un"),
-    (False, False, "An", False, False, "Un"),
-    (False, False, "au", False, False, "oo"),
-    (False, False, "Au", False, False, "Oo"),
-    (False, False, "a", True, False, "e"),
-    (False, False, "A", True, False, "E"),
-    (False, False, "en", False, True, "ee"),
-    (True, False, "ew", False, False, "oo"),
-    (True, False, "e", False, True, "e-a"),
-    (False, True, "e", False, False, "i"),
-    (False, True, "E", False, False, "I"),
-    (True, False, "f", False, False, "ff"),
-    (True, False, "ir", False, False, "ur"),
-    (True, False, "i", False, False, "ee"),  # FIXME
-    (True, False, "ow", False, False, "oo"),
-    (False, True, "o", False, False, "oo"),
-    (False, True, "O", False, False, "Oo"),
-    (True, False, "o", False, False, "u"),
-    (False, False, "the", False, False, "zee"),
-    (False, False, "The", False, False, "Zee"),
-    (False, False, "th", False, True, "t"),
-    (True, False, "tion", False, False, "shun"),
-    (True, False, "u", False, False, "oo"),
-    (True, False, "U", False, False, "Oo"),
-    (False, False, "v", False, False, "f"),
-    (False, False, "V", False, False, "F"),
-    (False, False, "w", False, False, "v"),
-    (False, False, "W", False, False, "V")
+    # Anti-replacements: need these so that we make sure these words
+    # don't get screwed up by later rules.
+    (False, True, 'need', False, True, 'need'),
+    (False, True, 'Need', False, True, 'Need'),
+
+    # Replace entire words
+    (False, True, 'add-on', False, True, 'bilge rat'),
+    (False, True, 'add-ons', False, True, 'bilge rats'),
+    (False, True, 'are', False, True, 'bee'),
+    (False, True, 'browser', False, True, 'corsairr'),
+    (False, True, 'for', False, True, 'fer'),
+    (False, True, 'Hi', False, True, 'H\'ello'),
+    (False, True, 'my', False, True, 'me'),
+    (False, True, 'no', False, True, 'nay'),
+    (False, True, 'of', False, True, 'o\''),
+    (False, True, 'over', False, True, 'o\'err'),
+    (False, True, 'plugin', False, True, 'mug o\' grog'),
+    (False, True, 'plugins', False, True, 'mugs o\' grog'),
+    (False, True, 'program', False, True, 'Jolly Rogerr'),
+    (False, True, 'the', False, True, 'th\''),
+    (False, True, 'there', False, True, 'tharr'),
+    (False, True, 'want', False, True, 'wants'),
+    (False, True, 'where', False, True, '\'erre'),
+    (False, True, 'with', False, True, 'wit\''),
+    (False, True, 'yes', False, True, 'aye'),
+    (False, True, 'you', False, True, 'ye\''),
+    (False, True, 'You', False, True, 'Ye\''),
+    (False, True, 'your', False, True, 'yerr'),
+    (False, True, 'Your', False, True, 'Yerr'),
+
+    # Prefixes
+    (False, True, 'hel', True, False, '\'el'),
+    (False, True, 'Hel', True, False, '\'el'),
+
+    # Mid-word
+    (True, False, 'er', True, False, 'arr'),
+
+    # Suffixes
+    (True, False, 'a', False, True, 'ar'),
+    (True, False, 'ed', False, True, '\'d'),
+    (True, False, 'ing', False, True, 'in\''),
+    (True, False, 'ort', False, True, 'arrt'),
+    (True, False, 'r', False, True, 'rr'),
+    (True, False, 'w', False, True, 'ww'),
 )
 
 
-def chef_transform(s):
-    # old_s = s
+def pirate_transform(s):
+    """Transforms a string into Pirate.
+
+    :arg s: The string to transform.
+
+    :returns: Pirated string.
+
+    """
+    old_s = s
     out = []
 
     in_word = False  # in a word?
 
     # TODO: This is awful--better to do a real lexer
     while s:
-        if s.startswith((".", "!", "?")):
+        if s.startswith(('.', '!', '?')):
             in_word = False
             out.append(s[0])
             s = s[1:]
             continue
 
+        debug(s, in_word)
+
         for mem in TRANSFORM:
+            # Match inside a word? (Not a prefix.)
             if in_word and not mem[0]:
-                continue
-            if not in_word and mem[1]:
-                continue
-            if not s.startswith(mem[2]):
-                continue
-            try:
-                if mem[3] and not wc(s[len(mem[2])]):
-                    continue
-            except IndexError:
+                debug(mem, 'not in word')
                 continue
 
+            # Not match inside a word? (Prefix.)
+            if not in_word and not mem[1]:
+                debug(mem, 'in word')
+                continue
+
+            if not s.startswith(mem[2]):
+                debug(mem, 'not match')
+                continue
+
+            # Check the character after the match to see if it's a
+            # word character and whether this match covers that.
             try:
-                if mem[4] and not nwc(s[len(mem[2])]):
+                 # WC: word character
+                if mem[3] and not wc(s[len(mem[2])]):
+                    debug(mem, 'not wc')
                     continue
             except IndexError:
-                continue
+                # We don't count EOS as a word character.
+                if mem[3]:
+                    debug(mem, 'not wc')
+                    continue
+
+            # Check the character after the match to see if it's not a
+            # word character and whether this match covers that.
+            try:
+                # NW: not word character
+                if mem[4] and not nwc(s[len(mem[2])]):
+                    debug(mem, 'wc')
+                    continue
+            except IndexError:
+                # We count EOS as a non-word character.
+                if not mem[4]:
+                    debug(mem, 'wc')
+                    continue
 
             out.append(mem[5])
             s = s[len(mem[2]):]
@@ -114,28 +204,40 @@ def chef_transform(s):
             break
 
         else:
+            in_word = wc(s[0])
             out.append(s[0])
             s = s[1:]
 
     # print old_s, "->", out
-    return u"".join(out)
+    new_s = u''.join(out)
+
+    # This guarantees every translated string has changed and is
+    # longer than the previous string.
+    if (not is_whitespace(new_s)
+        and (old_s == new_s or len(old_s) == len(new_s))):
+
+        if wc(new_s[-1]):
+            new_s = new_s + u'\''
+        else:
+            new_s = new_s[:-1] + u'\'' + new_s[-1]
+    return new_s
 
 
 class HtmlAwareMessageMunger(HTMLParser.HTMLParser):
     def __init__(self):
         HTMLParser.HTMLParser.__init__(self)
-        self.s = ""
+        self.s = ''
 
     def result(self):
         return self.s
 
     def xform(self, s):
-        return chef_transform(s)
+        return pirate_transform(s)
 
     def handle_starttag(self, tag, attrs, closed=False):
-        self.s += "<" + tag
+        self.s += '<' + tag
         for name, val in attrs:
-            self.s += " "
+            self.s += ' '
             self.s += name
             self.s += '="'
             if name in ['alt', 'title']:
@@ -144,19 +246,20 @@ class HtmlAwareMessageMunger(HTMLParser.HTMLParser):
                 self.s += val
             self.s += '"'
         if closed:
-            self.s += " /"
-        self.s += ">"
+            self.s += ' /'
+        self.s += '>'
 
     def handle_startendtag(self, tag, attrs):
         self.handle_starttag(tag, attrs, closed=True)
 
     def handle_endtag(self, tag):
-        self.s += "</" + tag + ">"
+        self.s += '</' + tag + '>'
 
     def handle_data(self, data):
-        # We don't want to munge placeholders, so split on them, keeping them
-        # in the list, then xform every other token.
+        # We don't want to munge placeholders, so split on them,
+        # keeping them in the list, then xform every other token.
         toks = INTERP_RE.split(data)
+
         for i, tok in enumerate(toks):
             if i % 2:
                 self.s += tok
@@ -164,44 +267,104 @@ class HtmlAwareMessageMunger(HTMLParser.HTMLParser):
                 self.s += self.xform(tok)
 
     def handle_charref(self, name):
-        self.s += "&#" + name + ";"
+        self.s += '&#' + name + ';'
 
     def handle_entityref(self, name):
-        self.s += "&" + name + ";"
+        self.s += '&' + name + ';'
+
+
+def split_ending(s):
+    ending = []
+    while s:
+        if s[-1] in '.,":;?!':
+            ending.insert(0, s[-1])
+            s = s[:-1]
+        else:
+            return s, u''.join(ending)
+
+    return u'', ''.join(ending)
 
 
 def translate_string(s):
+    # If it consists solely of whitespace, skip it.
+    if is_whitespace(s):
+        return s
+
     hamm = HtmlAwareMessageMunger()
     hamm.feed(s)
     out = hamm.result()
 
-    if out.endswith(" >"):
-        return out[:-2] + u" börk! >"
-    elif out.endswith("\n"):
-        return out[:-2] + u" börk börk börk!\n"
-    return out + u" börk!"
+    # Add color which causes every string to be longer.
+    s, ending = split_ending(out)
+    out = s + u' ' + COLOR[len(out) % len(COLOR)] + ending
+
+    # This guarantees that every string has at least one
+    # unicode charater
+    if '!' not in out:
+        out = out + u'!'
+
+    # Replace all ! with related unicode character.
+    out = out.replace(u'!', u'\u2757')
+    return out
 
 
 def munge_one_file(fname):
     po = polib.pofile(fname)
-    po.metadata["Language"] = "Swedish Chef"
-    po.metadata["Plural-Forms"] = "nplurals=2; plural= n != 1"
-    po.metadata["Content-Type"] = "text/plain; charset=UTF-8"
+    po.metadata['Language'] = 'Pirate'
+    po.metadata['Plural-Forms'] = 'nplurals=2; plural= n != 1'
+    po.metadata['Content-Type'] = 'text/plain; charset=UTF-8'
     count = 0
     for entry in po:
         if entry.msgid_plural:
-            entry.msgstr_plural["0"] = translate_string(entry.msgid)
-            entry.msgstr_plural["1"] = translate_string(entry.msgid_plural)
+            entry.msgstr_plural['0'] = translate_string(entry.msgid)
+            entry.msgstr_plural['1'] = translate_string(entry.msgid_plural)
         else:
             entry.msgstr = translate_string(entry.msgid)
 
         if 'fuzzy' in entry.flags:
             entry.flags.remove('fuzzy')  # clear the fuzzy flag
         count += 1
-    print "Munged %d messages in %s" % (count, fname)
+    print 'Munged %d messages in %s' % (count, fname)
     po.save()
 
 
-if __name__ == "__main__":
-    for fname in sys.argv[1:]:
-        munge_one_file(fname)
+def run_tests():
+    for mem in [
+        'Hello %s',
+        'Hello %(username)s',
+        'Hello %(user)s%(name)s',
+        'Hello {username}',
+        'Hello {user}{name}',
+        'Products and Services',
+        'Get community support',
+        'Your input helps make Mozilla better.',
+        'Super browsing',
+        ]:
+        print repr(mem), '->', repr(translate_string(mem))
+
+    return 0
+
+
+if __name__ == '__main__':
+    parser = optparse.OptionParser(usage=USAGE)
+    parser.add_option('--test', action='store_true', dest='runtests',
+                      default=False,
+                      help='runs tests')
+    parser.add_option('-s', '--string', action='store_true', dest='strings',
+                      help='translates specified string args')
+    (options, args) = parser.parse_args()
+
+    if options.runtests:
+        sys.exit(run_tests())
+
+    if not args:
+        parser.print_help()
+        sys.exit(1)
+
+    if options.strings:
+        for arg in args:
+            print translate_string(arg)
+        sys.exit(0)
+
+    for arg in args:
+        munge_one_file(arg)
