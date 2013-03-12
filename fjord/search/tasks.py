mozilla/fjord

commit 905dca6c840d69c85498ed238aae2ff7032a95b6

Test celery failure sends email
Note: This explicitly adds a bug to the celery index_item_task so as
to verify that deployments are working correctly on -dev and also
that celery workers are set up correctly to send error email.

fjord/search/tasks.py
@@ -25,6 +25,7 @@ def index_chunk_task(index, batch_id, rec_id, chunk):
     :arg chunk: a (class, id_list) of things to index
     """
     cls, id_list = chunk
+    rec = None
 
     try:
         # Pin to master db to avoid replication lag issues and stale
@@ -43,8 +44,9 @@ def index_chunk_task(index, batch_id, rec_id, chunk):
         rec.mark_success()
 
     except Exception:
-        rec.mark_fail(u'Errored out %s %s' % (
-                sys.exc_type, sys.exc_value))
+        if rec is not None:
+            rec.mark_fail(u'Errored out %s %s' % (
+                    sys.exc_type, sys.exc_value))
         raise
 
     finally:
@@ -70,7 +72,7 @@ def index_item_task(mapping_type, item_id, **kwargs):
     log.debug('Index attempt #%s', retries)
     try:
         doc = mapping_type.extract_document(item_id)
-        mapping_type.index(doc, item_id)
+        mapping_type.index(dock, item_id)
 
     except Exception as exc:
         log.exception("Error while live indexing %s %d: %s",

commit 287598d39a8829327e6c23453062c515e7069779

Fix bug introduced in 905dca6

fjord/search/tasks.py
@@ -72,7 +72,7 @@ def index_item_task(mapping_type, item_id, **kwargs):
     log.debug('Index attempt #%s', retries)
     try:
         doc = mapping_type.extract_document(item_id)
-        mapping_type.index(dock, item_id)
+        mapping_type.index(doc, item_id)
 
     except Exception as exc:
         log.exception("Error while live indexing %s %d: %s",