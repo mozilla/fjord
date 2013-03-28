684dc38622d6cbe70879fb900ce5d73146a0cb40
Will Kahn-Greene <willkg@mozilla.com>
2013-03-28 13:30:12 -0400

    Nix helpful admin login requirement
    Pretty sure this is causing the redirect that's halting bug #853644.

--- fjord/urls.py
+++ fjord/urls.py
@@ -1,6 +1,5 @@
 from django.conf import settings
 from django.conf.urls.defaults import patterns, include
-from django.contrib.auth.decorators import login_required
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.http import HttpResponse
 
@@ -12,7 +11,6 @@ from adminplus import AdminSitePlus
 
 admin.site = AdminSitePlus()
 admin.autodiscover()
-admin.site.login = login_required(admin.site.login)
 
 
 urlpatterns = patterns('',
