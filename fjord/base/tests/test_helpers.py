32faec230038f4f91de7db3ee42b0ef5c4488363
Will Kahn-Greene <willkg@mozilla.com>
2013-04-08 10:36:54 -0400

    Add convert_date_string helper

--- fjord/base/tests/test_helpers.py
+++ fjord/base/tests/test_helpers.py
@@ -1,6 +1,6 @@
 from nose.tools import eq_
 
-from fjord.base.helpers import locale_name
+from fjord.base.helpers import locale_name, convert_date_string
 from fjord.base.tests import TestCase
 
 
@@ -15,3 +15,20 @@ class TestLocaleName(TestCase):
         eq_(locale_name('en-US', native=True), u'English (US)')
         eq_(locale_name('es', native=True), u'Espa\u00f1ol')
         eq_(locale_name('gd', native=True), u'G\u00e0idhlig')
+
+
+class TestConvertDateString(TestCase):
+    def test_bad_input(self):
+        self.assertRaises(TypeError, lambda: convert_date_string(None))
+        self.assertRaises(ValueError, lambda: convert_date_string(''))
+        self.assertRaises(
+            ValueError,
+            lambda: convert_date_string('2013-04-03', in_fmt='%H:%M:%S'))
+
+    def test_convert_date_string(self):
+        eq_(convert_date_string('2013-04-03T14:42:15'), '2013-04-03')
+        eq_(convert_date_string(u'2013-04-03T14:42:15'), '2013-04-03')
+
+    def test_fmts(self):
+        eq_(convert_date_string(u'2013-04-03', in_fmt='%Y-%m-%d'), '2013-04-03')
+        eq_(convert_date_string(u'2013-04-03T14:42:15', out_fmt='%Y'), '2013')
