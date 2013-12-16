fd7c877c2401d6aca9b92d8532aa84f411ad18f9
Will Kahn-Greene <willkg@mozilla.com>
2013-12-16 16:45:00 -0500

    [bug 931189] Add character counter to mobile feedback form
    This adds a character counter to the mobile feedback form. This doesn't
    prevent users from submitting the form--it just indicates how many
    characters are acceptable, when the user is getting close to the limit
    and when the user is over the limit.
    
    Also, I moved part of the privacy text upwards so it's right under the
    description. This is not a change that requires l10n work. It is a
    change that the Privacy folks suggested in bug 934721 for the other
    feedback form.

--- fjord/feedback/static/js/mobile/feedback.js
+++ fjord/feedback/static/js/mobile/feedback.js
@@ -1,4 +1,19 @@
-(function() {
+(function($) {
+    function countRemaining(inputElement) {
+        // Updates the counter and inputElement based on the number of
+        // characters in the specified inputElement.
+        var $desc = $(inputElement);
+        var $counter = $('#' + $desc.attr('id') + '-counter');
+        var max = $desc.attr('data-max-length');
+        var remaining = max - $desc.val().length;
+
+        $desc.toggleClass('error', remaining < 0);
+
+        $counter.text(remaining);
+        $counter.toggleClass('error', remaining < 0);
+        $counter.toggleClass('warning', (remaining >= 0 && remaining <= Math.round(max * 0.2)));
+    }
+
     function init() {
         $('#intro').on('click', 'button', function() {
             var happy = $(this).hasClass('happy');
@@ -32,9 +47,16 @@
             $('#moreinfo').addClass('next');
         }
 
+        $('article textarea').each(function(i, element) {
+            countRemaining(element);
+        });
+        $('article textarea').keyup(function() {
+            countRemaining(this);
+        });
+
         $('#intro, #moreinfo').show();
     }
 
     $(init);
 
-})();
+}(jQuery));
