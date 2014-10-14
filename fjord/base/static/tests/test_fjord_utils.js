2cd62ad18c108dd33b97d2341bded5f785a721a6
Adam Okoye <aokoye@gmail.com>
2014-10-14 15:47:44 -0700

    [bug 1081413] Add test for url validation

--- fjord/base/static/tests/test_fjord_utils.js
+++ fjord/base/static/tests/test_fjord_utils.js
@@ -15,6 +15,27 @@ test("basic email validation", function() {
     }
 });
 
+test("basic url validation", function() {
+    var i;
+    var test_data = [
+        ['a', false],
+        ['about:start', false],
+        ['chrome://somepage', false],
+        ['http://example', false],
+        ['', true],
+        ['example.com', true],
+        ['ftp://example.com', true],
+        ['http://example.com', true],
+        ['https://example.com', true],
+        ['https://foo.example.com:8000/blah/blah/?foo=bar#baz', true],
+        ['http://mozilla.org/\u2713', true]
+    ];
+
+    for (i=0; i < test_data.length; i++) {
+        var item = test_data[i];
+        ok(fjord.validateUrl(item[0]) == item[1], item);
+    }
+});
 // FIXME: This causes qunit to reload the page which causes problems.
 // It'd be nice if we could test this without causing problems.
 // test("set and get querystring", function() {
