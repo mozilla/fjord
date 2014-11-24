f8432863c98fb7e126195702a2bdd70011f75dbc
Will Kahn-Greene <willkg@mozilla.com>
2014-11-24 18:37:39 -0500

    Fix compile-linted-mo.sh to take pythonbin as arg

--- bin/compile-linted-mo.sh
+++ bin/compile-linted-mo.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # Run this from the project root--not from this directory!
+#
+# usage: compile-linted-mo.sh [-p <PYTHON>]
 
 function compilemo() {
     dir=`dirname $1`
@@ -9,6 +11,15 @@ function compilemo() {
     echo "Compiling $1..."
     msgfmt -o "${dir}/${stem}.mo" "$1"
 }
+
+# Get the path to the python binary from the "-p <PYTHON>"
+# arg or default to "python2.6".
+if [ "$1" = "-p" ]
+then
+    PYTHON="$2"
+else
+    PYTHON="python2.6"
+fi
 
 NO_MO_FILES=();
 GOOD_FILES=();
@@ -27,7 +38,7 @@ do
         NO_MO_FILES+=("${pofile}")
 
     else
-        ./manage.py lint --errorsonly "${pofile}"
+        "${PYTHON}" manage.py lint --errorsonly "${pofile}"
         if [ $? -ne 0 ]
         then
             BAD_FILES+=("${pofile}")
