966da77481fa749f1df7aeec75ea20be55bcdd79
Will Kahn-Greene <willkg@mozilla.com>
2014-11-25 13:45:31 -0500

    Add celery health check
    * tweak the celery settings admin view to have a health check button
    * the health check button kicks off a task that emails admin with some
      celery health details

--- fjord/base/admin.py
+++ fjord/base/admin.py
@@ -1,12 +1,16 @@
 import re
 import sys
+from datetime import datetime
 
 from django import VERSION
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views import debug
 
 from celery import current_app
+
+from .tasks import celery_health_task
 
 
 def settings_view(request):
@@ -26,25 +30,32 @@ admin.site.register_view(path='settings',
                          view=settings_view)
 
 
-def celery_settings_view(request):
-    """Admin view that displays the celery configuration."""
+def celery_health_view(request):
+    """Admin view that displays the celery configuration and health."""
+    if request.method == 'POST':
+        celery_health_task.delay(datetime.now())
+        messages.success(request, 'Health task created.')
+        return HttpResponseRedirect(request.path)
+
     capital = re.compile('^[A-Z]')
     settings = [key for key in dir(current_app.conf) if capital.match(key)]
     sorted_settings = [
-        {'key': key,
-         'value': ('*****' if 'password' in key.lower()
-                   else getattr(current_app.conf, key))}
-        for key in sorted(settings)]
+        {
+            'key': key,
+            'value': ('*****' if 'password' in key.lower()
+                      else getattr(current_app.conf, key))
+        } for key in sorted(settings)
+    ]
 
-    return render(request, 'admin/settings_view.html', {
+    return render(request, 'admin/celery_health_view.html', {
         'settings': sorted_settings,
-        'title': 'Celery Settings'
+        'title': 'Celery Settings and Health'
     })
 
 
 admin.site.register_view(path='celery',
-                         name='Settings - Celery Settings',
-                         view=celery_settings_view)
+                         name='Settings - Celery Settings and Health',
+                         view=celery_health_view)
 
 
 def env_view(request):
---
+++ fjord/base/tasks.py
@@ -0,0 +1,30 @@
+import sys
+from datetime import datetime
+
+from django.core.mail import mail_admins
+
+from celery import task, VERSION
+
+
+@task()
+def celery_health_task(creation_dt):
+    """Emails celery health"""
+    now = datetime.now()
+
+    data = [
+        ('creation_dt', creation_dt),
+        ('execute_dt', now),
+        ('delta', now - creation_dt),
+        ('python_ver', sys.version),
+        ('celery_ver', VERSION),
+    ]
+
+    message = '\n'.join(['{key}: {val}'.format(key=key, val=str(val))
+                         for key, val in data])
+
+    message += '\n\nThank you for playing!\n'
+
+    mail_admins(
+        subject='celery health: {0}'.format(now),
+        message=message
+    )
