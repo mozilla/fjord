ae70dff0fddd580c82f744b0a450e489092014ac
Will Kahn-Greene <willkg@mozilla.com>
2015-01-27 22:25:08 -0500

    Add tests for new reverse
    This adds the tests that should have gone with commit 16250bf.

---
+++ fjord/base/tests/test_urlresolvers.py
@@ -0,0 +1,18 @@
+from nose.tools import eq_
+
+from fjord.base.tests import TestCase
+from fjord.base.urlresolvers import reverse
+
+
+class TestReverse(TestCase):
+    def test_no_locale(self):
+        # Note: This depends on the 'about' view. If we change that,
+        # then it breaks this test. But it seems silly to do a bunch
+        # of work to set up a better less fragile test plus it's
+        # unlikely we'll change the 'about' view.
+        eq_(reverse('about-view'), '/en-US/about')
+
+    def test_locale(self):
+        # Note: This depends on the 'about' view and the 'es'
+        # locale. If we change those, then it breaks this test.
+        eq_(reverse('about-view', locale='es'), '/es/about')
