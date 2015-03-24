99c84d32b13e2ffe3dbc0a08eb9ddb92e59d305f
Will Kahn-Greene <willkg@mozilla.com>
2015-03-24 10:31:39 -0400

    [bug 1137022] Minor settings cleanup
    * remove some more settings that aren't used anywhere (DOMAIN, PROTOCOL,
      PORT)
    * move NOSE_ARGS settings from local.py-dist to test.py where they
      really should be
    * (cosmetic) fix a typo in requirements.txt

--- fjord/settings/test.py
+++ fjord/settings/test.py
@@ -1,3 +1,13 @@
 DEBUG = TEMPLATE_DEBUG = True
 CELERY_ALWAYS_EAGER = True
 SESSION_COOKIE_SECURE = False
+
+# This sets some nose flags otherwise testing is very noisy. Comment
+# these out if you need the information they're quelling.
+#
+# See http://fjord.readthedocs.org/en/latest/testing.html
+NOSE_ARGS = [
+    '-s',                       # Don't ask any questions
+    '--nocapture',              # Don't capture stdout
+    '--logging-clear-handlers'  # Clear all other logging handlers
+]
