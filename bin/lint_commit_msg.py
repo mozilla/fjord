c602d9bbcd187b5568e53540cd64f7752274ac53
Will Kahn-Greene <willkg@mozilla.com>
2015-03-27 09:46:38 -0400

    Rework commit message linter
    The commit message linter looked at changed code if I used -v when doing
    git commit. Further, if the commit message was rejected because of
    errors, there was no way to re-edit it without shenanigans.
    
    This reworks it so that it asks you if you want to re-edit the message.
    Plus it gives you better information about where the error is. Plus it
    ignores everything after the # ... stuff.

--- bin/lint_commit_msg.py
+++ bin/lint_commit_msg.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 
-import sys
+import os
 import re
+import subprocess
+import sys
 
 import requests
 from requests.exceptions import (
@@ -13,13 +15,14 @@ BUG_PREFIX_REGEX = r'\[bug (\d+)\]'
 
 
 def are_lines_not_more_than_79_chars(contents):
-    long_lines = [line for line in contents if len(line) > 79]
-    if long_lines:
-        print('Error:')
-        print('The commit message should not have more than '
-              '79 characters per line.')
-        return False
-    return True
+    errors = []
+    for lineno, line in enumerate(contents):
+        if len(line) > 79:
+            errors.append(
+                '%s: Line is longer than 79 characters.' % (lineno + 1)
+            )
+
+    return errors
 
 
 def is_bug_number_in_right_format(contents):
@@ -30,10 +33,8 @@ def is_bug_number_in_right_format(contents):
                                       flags=re.IGNORECASE)
         valid_bug_format = bug_format_regex.match(summary)
         if not valid_bug_format:
-            print('Specify the bug number in the format '
-                  '[bug xxxxxxx] at the beginning of the commit summary.')
-            return False
-    return True
+            return ['1: Bug number is in wrong format. Use [bug xxxxxx].']
+    return []
 
 
 def print_bug_info(contents):
@@ -45,21 +46,21 @@ def print_bug_info(contents):
         bug_id = is_a_bug.group(1)
         url = 'https://bugzilla.mozilla.org/rest/bug/{}'.format(bug_id)
         try:
-            print('Looking up bug {}...'.format(bug_id))
+            print 'Looking up bug {}...'.format(bug_id)
             response = requests.get(url, timeout=60.0)
             response_dict = response.json()
             if ('error' in response_dict and response_dict['error']):
-                print('Bug not found!'.format(bug_id))
-                return False
-            print('Found!')
+                print('Bug not found!')
+                return []
+            print 'Found!'
             bug_info = response_dict['bugs'][0]
-            print('    Summary: {}'.format(bug_info['summary']))
-            print('    Assigned to: {}'.format(bug_info['assigned_to']))
+            print '    Summary: {}'.format(bug_info['summary'])
+            print '    Assigned to: {}'.format(bug_info['assigned_to'])
         except ValueError:
-            print('Error parsing response from Bugzilla REST API')
+            print 'Error parsing response from Bugzilla REST API.'
         except (ConnectionError, Timeout):
-            print('Unable to contact Mozilla Bugzilla')
-    return True
+            print 'Unable to contact Mozilla Bugzilla.'
+    return []
 
 
 LINT_FUNCTIONS = [
@@ -69,20 +70,77 @@ LINT_FUNCTIONS = [
 ]
 
 
-def lint_commit_msg(commit_msg_file):
-    with open(commit_msg_file) as commit_contents:
-        commit_msg = [line for line in commit_contents.readlines()
-                      if not line.strip().startswith('#')]
-        if commit_msg:
-            return_values = map(lambda x: x(commit_msg), LINT_FUNCTIONS)
-            if not all(return_values):
-                return 1
+HEADER = '# COMMIT MESSAGE ERRORS:'
+
+
+def lint_commit_msg(commit_msg_file, editor):
+    while True:
+        commit_msg = []
+
+        with open(commit_msg_file) as fp:
+            for line in fp:
+                # Only strip the right side. That way we don't screw
+                # up any indentation from the original message.
+                line = line.rstrip()
+
+                # Break out of loop if we hit the error header or the
+                # git commit message instructions.
+                if line == HEADER or line.startswith('# '):
+                    break
+
+                commit_msg.append(line)
+
+        # Lint functions should return lists of strings where each
+        # string is an error message. If they have warnings, they
+        # should just print the warnings to stdout.
+        errors = []
+        for func in LINT_FUNCTIONS:
+            errors.extend(func(commit_msg))
+
+        if not errors:
+            return 0
+
+        print 'Invalid git commit message:'
+        print ''
+        print '\n'.join(errors)
+        print ''
+        print (
+            'Would you like to edit the message?\n'
+            '* y (yes): edit commit with $EDITOR\n'
+            '* n (no): the commit will fail\n'
+            '* i (ignore): the commit will succeed as is'
+        )
+        print ''
+        ret = raw_input('Edit commit message? [Y/n/i] ')
+
+        ret = ret.strip()
+        ret = ret.lower().strip()[:1]
+        if not ret or ret == 'n':
+            print 'Rejecting commit message...'
+            return 1
+        if ret == 'i':
+            print 'Ignoring errors...'
+            return 0
+
+        commit_msg.append(HEADER)
+        commit_msg.extend(['# ' + error for error in errors])
+
+        # Rewrite the commit message file with the errors. Then
+        # start the editor with it.
+        with open(commit_msg_file, 'w') as fp:
+            fp.write('\n'.join(commit_msg))
+
+        subprocess.call('%s %s' % (editor, commit_msg_file), shell=True)
+
+        # Go through the loop again.
 
     return 0
 
 
 if __name__ == '__main__':
     commit_msg_file = sys.argv[1]
-    errors_found = lint_commit_msg(commit_msg_file)
+    editor = os.environ['EDITOR']
+    errors_found = lint_commit_msg(commit_msg_file, editor)
+
     if errors_found:
         sys.exit(1)
