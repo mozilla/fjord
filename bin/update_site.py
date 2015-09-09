7c141e2f9e5b1207d5027621b196ce8392c80df1
Will Kahn-Greene <willkg@mozilla.com>
2015-09-09 09:33:25 -0400

    [bug 1202288] Remove jingo-minify
    I don't know why it was still around--we don't use it anymore.

--- bin/update_site.py
+++ bin/update_site.py
@@ -89,9 +89,6 @@ def update_site(env, debug):
         (CHDIR, os.path.join(here)),
         (EXEC, 'python2.6 manage.py migrate --all'),
         (EXEC, 'python2.6 manage.py collectstatic --noinput'),
-        # un-comment if you you're using jingo-minify and not
-        # django-compressor
-        (EXEC, 'python2.6 manage.py compress_assets'),
     ]
 
     for cmd, cmd_args in commands:
