519fd2077ffe569e4f8baec5b7ef475e8cdef421
hoosteeno <justin@tentacu.com>
2016-04-22 14:58:14 -0600

    [bug 1253423] Fix footer; handle click event instead of triggering.

--- fjord/feedback/static/js/android_feedback.js
+++ fjord/feedback/static/js/android_feedback.js
@@ -1,51 +1,62 @@
-(function ($) {
+(function ($, cards) {
     'use strict';
 
-    // Settings
-    var browserReviewUrl = 'https://play.google.com/store/apps/details?id=org.mozilla.CHANNEL#details-reviews';
-    var deviceReviewUrl = 'market://details?id=org.mozilla.CHANNEL';
+    $(document).ready(function() {
 
-    // Initialize
-    var reviewUrl = browserReviewUrl;
-    var channel = $('body').attr('data-channel');
+        // Settings
+        var browserReviewUrl = 'https://play.google.com/store/apps/details?id=org.mozilla.CHANNEL#details-reviews';
+        var deviceReviewUrl = 'market://details?id=org.mozilla.CHANNEL';
 
-    /*
-      Fx Android fires an event when loaded by feedback prompt;
-      in that case act like we're on a device.
+        // Initialize
+        var reviewUrl = browserReviewUrl;
+        var channel = $('body').attr('data-channel');
 
-      Simulate this in the console: Services.obs.notifyObservers(null, "Feedback:Show", null);
-    */
-    document.addEventListener('FeedbackPrompted', function(event) {
-        reviewUrl = deviceReviewUrl;
-        $('a.maybe-later, a.no-thanks').show();
+        /*
+          Fx Android fires an event when loaded by feedback prompt;
+          in that case act like we're on a device.
 
-        // If client prompted for feedback, delegate some event handling to the client
-        $(document).on('click', 'a.maybe-later, a.no-thanks', function(e) {
-            e.preventDefault();
-            var eventName = this.getAttribute('data-event');
-            var feedbackEvent = new Event(eventName, {'bubbles':true, 'cancelable':false});
-            this.dispatchEvent(feedbackEvent);
+          Simulate this in the console: Services.obs.notifyObservers(null, "Feedback:Show", null);
+        */
+        document.addEventListener('FeedbackPrompted', function(event) {
+            reviewUrl = deviceReviewUrl;
+            $('a.maybe-later, a.no-thanks').show();
+
+            // If client prompted for feedback, delegate some event handling to the client
+            $(document).on('click', 'a.maybe-later, a.no-thanks', function(e) {
+                e.preventDefault();
+                var eventName = this.getAttribute('data-event');
+                var feedbackEvent = new Event(eventName, {'bubbles':true, 'cancelable':false});
+                this.dispatchEvent(feedbackEvent);
+            });
         });
-    });
 
-    $(document).on('click', '#happy-button-android', function(e) {
-      // Play store only has beta/release
-        if (channel != '' && /^(beta|release).*$/.test(channel)) {
-            if (channel == 'beta') {
-                reviewUrl = reviewUrl.replace('CHANNEL', 'firefox_beta');
+        $(document).on('click', '#happy-button-android', function(e) {
+          // Play store only has beta/release
+            if (channel != '' && /^(beta|release).*$/.test(channel)) {
+                if (channel == 'beta') {
+                    reviewUrl = reviewUrl.replace('CHANNEL', 'firefox_beta');
+                }
+                else {
+                    reviewUrl = reviewUrl.replace('CHANNEL', 'firefox');
+                }
+                $('#play-store').attr('href', reviewUrl);
             }
             else {
-                reviewUrl = reviewUrl.replace('CHANNEL', 'firefox');
+                // If not beta/release, hide CTA to review on play store
+                $('.play-store-cta').hide();
             }
-            $('#play-store').attr('href', reviewUrl);
-        }
-        else {
-            // If not beta/release, hide CTA to review on play store
-            $('.play-store-cta').hide();
-        }
 
-        // Trigger input's generic_feeback.js handler
-        $('#happy-button').click();
+            $('.happy').show();
+            $('.sad').hide();
+            cards.changeCard('forward');
+            return false;
+        });
+
+        $(document).on('keypress', '#happy-button-android', function(e) {
+            if ((e.keyCode || e.which) === '13') {
+                $('#happy-button-android').click();
+            }
+        });
+
     });
-
-}(jQuery));
+}(jQuery, cards));
